2502df459dc833788d1f37b4dcdae4eed27cbd5e
eijjeka <eijjeka@gmail.com>
2025-06-17 13:52:21 +0300

    changed send-mail

--- src/send_mail.php
+++ src/send_mail.php
@@ -1,62 +1,75 @@
 <?php
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
 
-    require "PHPMailer/src/Exception.php";
-    require "PHPMailer/src/PHPMailer.php";
+$config = require __DIR__ . '../../../config.php';
 
-    $mail = new PHPMailer(true); /* Создаем объект MAIL */
-$mail->CharSet = "UTF-8"; /* Задаем кодировку UTF-8 */
-$mail->IsHTML(true); /* Разрешаем работу с HTML */
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
 
-/* Отримуємо JSON з тіла запиту */
-$data = json_decode(file_get_contents("php://input"), true);
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
 
-/* Перевіряємо, чи вдалося розпарсити JSON */
-if (!$data) {
-    echo json_encode(["message" => "Помилка: Некоректний JSON"]);
-    exit;
+$mail = new PHPMailer(true);
+
+try {
+    $mail->CharSet = "UTF-8";
+    $mail->isHTML(true);
+
+    // Налаштування SMTP
+    $mail->isSMTP();
+    $mail->Host = 'mail.adm.tools';
+    $mail->SMTPAuth = true;
+    $mail->Username = $config['mail_username'];
+    $mail->Password = $config['mail_password'];
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // або PHPMailer::ENCRYPTION_STARTTLS
+    $mail->Port = 465; // або 587 для TLS
+
+    // Відправник
+    $mail->setFrom('info@avtoshkoladriver.com.ua', 'Автошкола Driver');
+
+    // Обробка JSON-даних
+    $data = json_decode(file_get_contents("php://input"), true);
+    if (!$data) {
+        echo json_encode(["message" => "Помилка: Некоректний JSON"]);
+        exit;
+    }
+
+    $name = $data["name"] ?? "";
+    $phone = $data["phone"] ?? "";
+    $driving = !empty($data["driving"]);
+    $history = !empty($data["history"]);
+
+    $drivingStatus = $driving ? '<span style="color: green;">&#10004;</span>' : '<span style="color: red;">&#10008;</span>';
+    $historyStatus = $history ? '<span style="color: green;">&#10004;</span>' : '<span style="color: red;">&#10008;</span>';
+
+    // Завантаження шаблону
+    $body = file_get_contents("template_mail.html");
+    if ($body === false) {
+        echo json_encode(["message" => "Помилка: Не вдалося прочитати шаблон email."]);
+        exit;
+    }
+
+    $body = str_replace('%name%', $name, $body);
+    $body = str_replace('%phone%', $phone, $body);
+    $body = str_replace('%driving%', $drivingStatus, $body);
+    $body = str_replace('%history%', $historyStatus, $body);
+
+    // Адресат і тема
+//    $mail->addAddress("eijjeka@gmail.com");
+    $mail->addAddress("avtoshkoladriver.tet@gmail.com");
+    $mail->addAddress("solodovnik.denis123@gmail.com");
+//    $mail->addAddress("kashirin.alexsandr91@gmail.com");
+    $mail->Subject = "[Заявка з форми]";
+    $mail->MsgHTML($body);
+
+    // Надсилання
+    $mail->send();
+    $message = "Заявка успішно відправлена, очікуйте дзвінка";
+} catch (Exception $e) {
+    $message = "Виникла помилка: " . $mail->ErrorInfo;
 }
 
-$name = isset($data["name"]) ? $data["name"] : "";
-$phone = isset($data["phone"]) ? $data["phone"] : "";
-$driving = isset($data["driving"]) ? (bool)$data["driving"] : false;
-$history = isset($data["history"]) ? (bool)$data["history"] : false;
-
-// Формування символів для driving та history
-$drivingStatus = $driving ? '<span style="color: green;">&#10004;</span>' : '<span style="color: red;">&#10008;</span>';
-$historyStatus = $history ? '<span style="color: green;">&#10004;</span>' : '<span style="color: red;">&#10008;</span>';
-
-
-$email_template = "template_mail.html"; // Считываем файл разметки
-$body = file_get_contents($email_template);
-if ($body === false) {
-    echo json_encode(["message" => "Помилка: Не вдалося прочитати шаблон email."]);
-    exit;
-} // Сохраняем данные в $body
-
-$body = str_replace('%name%', $name, $body); // Заменяем строку %name% на имя
-$body = str_replace('%phone%', $phone, $body); // Заменяем строку %phone% на телефон
-$body = str_replace('%driving%', $drivingStatus, $body); // Заменяем строку %coudrivingrse% на курс
-$body = str_replace('%history%', $historyStatus, $body); // Заменяем строку %history% на курс
-
-$mail->addAddress("kashirin.alexsandr91@gmail.com"); /* Здесь введите Email, куда отправлять */
-// $mail->addAddress("avtoshkoladriver.tet@gmail.com"); /* Здесь введите Email, куда отправлять */
-// $mail->setFrom($email);
-$mail->Subject = "[Заявка с форми]"; /* Тема письма */
-$mail->MsgHTML($body);
-
-/* Проверяем отправлено ли сообщение */
-if (!$mail->send()) {
-  $message = "Виникла помилка спробуйте ще раз";
-} else {
-  $message = "Заявка успішно відправленна очікуйте дзвінка";
-}
-
-/* Возвращаем ответ */
-$response = ["message" => $message];
-
-/* Ответ в формате JSON */
+// Відповідь у форматі JSON
 header('Content-type: application/json');
-echo json_encode($response);
-?>
+echo json_encode(["message" => $message]);
